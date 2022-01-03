b61309bdc79212f12fc8658f54e95a61dbaa43ca
Azimecha <azimecha@outlook.com>
2022-01-02 21:27:59 -0800

    Fixed current channel sticking around visually after deletion

--- Azimecha.Stupidchat.ClientApp.DesktopGUI/ServerControl.axaml.cs
+++ Azimecha.Stupidchat.ClientApp.DesktopGUI/ServerControl.axaml.cs
@@ -172,7 +172,11 @@ namespace Azimecha.Stupidchat.ClientApp.DesktopGUI {
         private void OnChannelRemoved(IChannel chan) => Avalonia.Threading.Dispatcher.UIThread.InvokeAsync(() => {
             _ctlChannelsStack.Children.RemoveAll(_ctlChannelsStack.Children.OfType<Button>()
                 .Where(ctl => (ctl.Tag is IChannel chanCur) && chanCur.Info.ID == chan.Info.ID).ToArray());
+
             chan.InfoChanged -= _procOnChannelModified;
+
+            if (chan.Info.ID == _channel.Info.ID)
+                Channel = null;
         });
 
         private void OnServerErrored(IServer server, Exception ex) => Avalonia.Threading.Dispatcher.UIThread.InvokeAsync(() => {
